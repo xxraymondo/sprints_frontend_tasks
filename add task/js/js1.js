8daebfc726e25512abccf2898649637cd083e9a7
khaled yehia <63373489+xxraymondo@users.noreply.github.com>
2023-05-29 01:36:01 +0300

    update update btn

--- add task/js/js1.js
+++ add task/js/js1.js
@@ -1,10 +1,15 @@
 document.getElementById("taskPairortyValidation").style.display="none"
 document.getElementById("taskNameValidation").style.display="none"
-
+document.getElementById("taskPairortyValidationUpdate").style.display="none"
+document.getElementById("taskNameValidationUpdate").style.display="none"
+document.getElementById("DivForUpdate").style.display="none"
+let updateBtn=document.getElementById("updateBtn")
 let tableData=[]
 let tablerow
 let taskName
 let taskPaiorty
+let taskPaiortyUpdate
+let taskNameUpdate
 function hideValidation(){
   
   document.getElementById("taskPairortyValidation").style.display="none"
@@ -23,7 +28,7 @@ if(taskName==""){
 }else{
     hideValidation()
   
-   tablerow = {id:tableData.length+1, taskName:taskName, taskPaiorty:taskPaiorty};
+   tablerow = {id:tableData.length, taskName:taskName, taskPaiorty:taskPaiorty};
 
     tableData.push(tablerow)
   console.log(tableData)
@@ -40,17 +45,31 @@ function displayData(){
     <td>${tableData[i].taskName}</td>
     <td>${tableData[i].taskPaiorty}</td>
     <td scope="col">
-    <button onclick="deleteRow(${i})" class="btn btn-danger">
-    delete
+  <button onclick="updateRow(${tableData[i].id})" class="btn btn-success">
+  update 
+</button>
+</td>
+    <td scope="col">
+    <button onclick="deleteRow(${tableData[i].id})" class="btn btn-danger">
+    delete 
   </button>
   </td>
+  
   </tr>`
   
   }
   document.getElementById("tableBody").innerHTML=htmlData
 }
+function clear(){
+  document.getElementById("taskName").value=""
+  document.getElementById("taskPaiorty").value=""
+  document.getElementById("taskNameUpdate").value=""
+  document.getElementById("taskPaiortyUpdate").value=""
+  
+}
 function addrowHtml(){
   addRow()
+  clear()
   displayData()
 }
 function deleteRow(x){
@@ -58,3 +77,29 @@ function deleteRow(x){
   tableData.splice(x,1)
   displayData()
 }
+
+
+var objIndex;
+function updateRow(x){
+
+  document.getElementById("DivForUpdate").style.display="block"
+  document.getElementById("DivForAdd").style.display="none"
+  objIndex = tableData.findIndex((obj => obj.id == x));
+  
+  
+  document.getElementById("taskNameUpdate").value =tableData[objIndex].taskName
+  document.getElementById("taskPaiortyUpdate").value = tableData[objIndex].taskPaiorty
+  
+
+}
+function executeUpdate(){
+ console.log( tableData[objIndex])
+  tableData[objIndex].taskName=document.getElementById("taskNameUpdate").value
+  tableData[objIndex].taskPaiorty=document.getElementById("taskPaiortyUpdate").value 
+  console.log(tableData[objIndex].taskName)
+  displayData()
+  document.getElementById("DivForUpdate").style.display="none"
+  document.getElementById("DivForAdd").style.display="block"
+  clear()
+}
+
