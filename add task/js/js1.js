29c7babf1d44cc34d8a40f937dc5658ec2e7c9dc
khaled yehia <63373489+xxraymondo@users.noreply.github.com>
2023-06-03 15:37:19 +0300

    update

--- add task/js/js1.js
+++ add task/js/js1.js
@@ -94,4 +94,73 @@ function showHighPiroty(){
   }
   return high
 }
+let boolName;
+let boolPaiorty;
+function sortByName(){
+  
+  boolName = !boolName
+  if(boolName==true){
+     x=tableData.sort((a, b) => a.taskName - b.taskName)
+  }else{
+  x.reverse()
+  }
+}
+function sortByPaiorty(){
+  boolPaiorty = !boolPaiorty
+  if(boolPaiorty==true){
+     x=tableData.sort((a, b) => a.taskPaiorty - b.taskPaiorty)
+  }else{
+  x.reverse()
+  }
+}
+document.getElementById("taskNameHead").addEventListener("click",function(){
+  sortByName()
+  displayData()
+})
 
+document.getElementById("taskPaiortyHead").addEventListener("click",function(){
+  sortByName()
+  displayData()
+})
+let tasksArray = [
+  ["Nathalie Nader Nabil", "Task 01", "Option 2"],
+  ["Nathalie Nader", "Task 01", "Option 1"],
+  ["Youssef Mohamed Ahmed Mohamed Youssef", "Task 01", "Option 1"],
+  ["Salma Nasreldin", "Task 01", "Option 1"],
+  ["Engy Mostafa", "Task 01", "Option 1"],
+  ["Engy Mostafa", "Task 01", "Option 1"],
+  ["Engy ahmed mostafa ", "Task 01", "Option 1"],
+  ["Abdelhay Nader Abdelhay Abozayed", "Task 01", "Option 1"],
+  ["Abdelrahman Shemies", "Task 01", "Option 1"],
+  ["Alaa Ahmed", "Task 01", "Option 2"],
+  ["Youssef Fathy Mahmoud", "Task 01", "Option 1"],
+  ["Mark Bassem", "Task 01", "Option 1"],
+  ["Anas Ahmed", "Task 01", "Option 1"],
+  ["Adham Hesham", "Task 01", "Option 1"],
+  ["Mohamed Ahmed Fahmi", "Task 01", "Option 1"],
+  ["rola wafi", "Task 01", "Option 1"],
+  ["Moataz Youssef", "Task 01", "Option 2"],
+  ["Ahmad Salama", "Task 01", "Option 1"],
+  ["Mohamed Ahmed Fahmi", "Task 01", "Option 1"],
+  ["Ahmad Salama Abdelaziz", "Task 01", "Option 2"],
+  ["Kareem Ramzi El-Tahlawi", "Task 01", "Option 1"],
+  ["Alaa Ahmed", "Task 01", "Option 2"],
+  ["rola wafi", "Task 01", "Option 2"],
+  ["Mohamed Fahmi", "Task 01", "Option 1"],
+  ["Mohamed Fahmi", "Task 01", "Option 2"],
+  ["Alaa Ahmed", "Task 01", "Option 2"],
+  ["Abdelrahman Shemies", "Task 01", "Option 1"],
+ 
+  ["Nathalie Nader", "Task 01", "Option 1"],
+
+  ["Nathalie Nader", "Task 01", "Option 1"],
+
+  ["Nathalie Nader", "Task 01", "Option 1"],
+
+  ["Mariam Ahmed", "Task 01", "Option 1"],
+  ["Mariam Ahmed", "Task 01", "Option 1"],
+  ["Mariam Ahmed", "Task 01", "Option 1"],
+];
+const unique = [...new Set(tasksArray.map(item => item[0]))]; // [ 'A', 'B']
+
+console.log(unique)
